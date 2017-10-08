0a7f7b5b37bdf34f9f93495569b9b5d74a3cf9fc
angel <githubUniversity@gmail.com>
2017-10-08 23:44:07 +0200

    CompanyBuilder

---
+++ src/main/java/builder/CompanyBuilder.java
@@ -0,0 +1,38 @@
+package builder;
+
+import models.Company;
+import models.Size;
+import models.Staff;
+
+public class CompanyBuilder {
+    
+    private Company company;
+    
+    public CompanyBuilder( int id ) {
+        this.company = new Company(id);
+    }
+    
+    public CompanyBuilder staff( Staff staff ) {
+        this.company.addStaff( staff );
+        return this;
+    }
+    
+    public CompanyBuilder name( String name ) {
+        this.company.setName( name );
+        return this;
+    }
+    
+    public CompanyBuilder address( String address ) {
+        this.company.setAddress( address );
+        return this;
+    }
+    
+    public CompanyBuilder size( Size size ) {
+        this.company.setSize( size );
+        return this;
+    }
+    
+    public Company build() {
+        return this.company;
+    }
+}
